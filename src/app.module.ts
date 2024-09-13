28e7d6c3eb6e65df38e9b855fea4adf3168487c1
leebhin <bini4039@gmail.com>
2024-09-13 10:03:52 +0900

    auth controller/service

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,8 @@ import { JwtModule } from '@nestjs/jwt';
 import { UserModule } from './user/user.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthController } from './auth/auth.controller';
+import { AuthService } from './auth/auth.service';
 
 @Module({
   imports: [
@@ -16,7 +18,7 @@ import { AppService } from './app.service';
     }),
     UserModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, AuthController],
+  providers: [AppService, AuthService],
 })
 export class AppModule { }
