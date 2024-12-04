ed3d8a5bfa3d408adb1bb0369a0ae21dd6ee8966
LeeBhin <bini4039@naver.com>
2024-12-04 16:21:36 +0900

    neo4j exception

--- src/neo4j/neo4j.module.ts
+++ src/neo4j/neo4j.module.ts
@@ -10,39 +10,39 @@ import { FriendsModule } from 'src/friends/friends.module';
 
 @Module({})
 export class Neo4jModule {
-    static forRootAsync(customConfig?: Neo4jConfig): DynamicModule {
-        return {
-            module: Neo4jModule,
-            imports: [ConfigModule, UserModule, FriendsModule],
-            global: true,
-            providers: [
-                QueryRepository,
-                {
-                    provide: NEO4J_CONFIG,
-                    inject: [ConfigService],
-                    useFactory: (configService: ConfigService) =>
-                        createDatabaseConfig(configService, customConfig),
-                },
-                {
-                    provide: NEO4J_CONNECTION,
-                    inject: [NEO4J_CONFIG],
-                    useFactory: async (config: Neo4jConfig) => {
-                        try {
-                            const { host, scheme, port, username, password } = config;
-                            const connection = new Connection(`${scheme}://${host}:${port}`, {
-                                username,
-                                password,
-                            }) as ConnectionWithDriver;
+  static forRootAsync(customConfig?: Neo4jConfig): DynamicModule {
+    return {
+      module: Neo4jModule,
+      imports: [ConfigModule, UserModule, FriendsModule],
+      global: true,
+      providers: [
+        QueryRepository,
+        {
+          provide: NEO4J_CONFIG,
+          inject: [ConfigService],
+          useFactory: (configService: ConfigService) =>
+            createDatabaseConfig(configService, customConfig),
+        },
+        {
+          provide: NEO4J_CONNECTION,
+          inject: [NEO4J_CONFIG],
+          useFactory: async (config: Neo4jConfig) => {
+            try {
+              const { host, scheme, port, username, password } = config;
+              const connection = new Connection(`${scheme}://${host}:${port}`, {
+                username,
+                password,
+              }) as ConnectionWithDriver;
 
-                            await (connection.driver as any).verifyConnectivity();
-                            return connection;
-                        } catch (error) {
-                            throw new ConnectionError(error);
-                        }
-                    },
-                },
-            ],
-            exports: [QueryRepository, NEO4J_CONNECTION],
-        };
-    }
+              await (connection.driver as any).verifyConnectivity();
+              return connection;
+            } catch (error) {
+              return null;
+            }
+          },
+        },
+      ],
+      exports: [QueryRepository, NEO4J_CONNECTION],
+    };
+  }
 }
