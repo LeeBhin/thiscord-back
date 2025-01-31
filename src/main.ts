2ef8ec5ccf457243e1731b02405d2775088c3ad9
Empty <46045967+LeeBhin@users.noreply.github.com>
2025-02-01 00:44:55 +0900

    CORS

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,11 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
   app.use(cookieParser());
-
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://smcthiscord.netlify.app'],
-    methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
+    origin: true,
     credentials: true,
   });
-
   await app.listen(3002);
 }
 bootstrap();
